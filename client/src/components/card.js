83298cb07bd1e58be505914f03dc7ef589682e50
Tushar Singh <tusharsingh432@gmail.com>
2022-05-29 19:28:30 +0530

    Front End and a little backend

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,16 +1,43 @@
 import './card.css';
 import ArmedPenguin from "../images/ArmedPenguin.jpg";
-
+import Modal from 'react-modal';
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 function Card() {
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const cardId = uuidv4();
+    function openModal() {
+        setIsOpen(true);
+    }
+    function afterOpenModal() {
+        subtitle.style.color = '#f00';
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    async function report(e) {
+        e.preventDefault();
+        try {
+            await axios.post('/api/report', { cardId });
+        } catch (e) {
+            alert("Report Failed");
+            console.log(e);
+        }
+        closeModal();
+    }
+
     return (
-        <div className='Card'>
+        <div className='Card' key={cardId}>
             <div className='CardTop'>
                 <div style={{ "minWidth": "70px", "display": "flex", "alignItems": "baseline", "justifyContent": "space-evenly" }}>
                     <i class="fa-brands fa-gg-circle"></i> tushar
                 </div>
-                <i class="fas fa-ellipsis-h"></i>
+                <i class="fas fa-ellipsis-h" onClick={openModal}></i>
             </div>
             <hr />
             <img className='CardImg' src={ArmedPenguin} alt='Armed Penguin' />
@@ -36,12 +63,23 @@ function Card() {
             <div className='CardComments'>
                 <i class="fa-regular fa-face-smile"></i>
                 &nbsp; &nbsp;
-                <span style={{ "color": "grey" }}>Add a comment...</span>
+                <input type="text" style={{ "color": "grey", "border": "none", "outline": "none" }} placeholder="Add a comment..." />
                 &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-                &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-                &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-                <span style={{ "color": "rgb(6,69,173, 0.5)" }}>POST</span>
+                &nbsp; &nbsp;&nbsp; &nbsp;
+                <span style={{ "color": "rgb(6,69,173, 0.5)" }}>Post</span>
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                className="Modal"
+            >
+                <form>
+                    <button className='ModalButtons' onClick={report} >Report</button>
+                    <hr />
+                    <button className='ModalButtons'>Unfollow</button>
+                </form>
+            </Modal>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.post('/api/report', (req, res) => {
+    console.log(req.body);
+
+    res.send("Success");
+})
+
 app.get('/', (req, res) => {
     res.send('Server Running///////');
 })
 
-app.listen(8080, () => {
+app.listen(8000, () => {
     console.log('listening on port 8080');
 })
